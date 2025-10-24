578222ac1da77624f566917c3e90ffcab874357e
Jhonata Costa <costajhonata8@gmail.com>
2025-10-24 14:36:02 -0300

    #001[feat] ~ Just the basics

---
+++ src/main/java/com/example/game_library_backend/GameLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_library_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/game_library_backend/GameLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_library_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
